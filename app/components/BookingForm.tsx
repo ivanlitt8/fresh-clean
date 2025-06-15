87f26d9de2d5a008339646ba6bb07ab5fdf94a05
Ivan <ivanlitt8@gmail.com>
2025-06-15 20:54:58 -0300

    fix build v2

--- app/components/BookingForm.tsx
+++ app/components/BookingForm.tsx
@@ -16,7 +16,7 @@ import {
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import { Upload } from "lucide-react";
-import { DateTimeSelector } from "@/app/components/DateTimeSelector";
+import { DateTimeSelector } from "./DateTimeSelector";
 
 interface BookingFormProps {
   currentStep: number;
